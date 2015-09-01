feb99d2c759ae588b4afedadabb6a37fcbf980d7
geekahedron <d_j_@hotmail.com>
2015-09-01 14:47:58 -0400

    Create gkh.user.js
    No content, basic headers

---
+++ gkh.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name	GKH for Utopia
+// @namespace	https://github.com/geekahedron/gopher-kingdom-haven/
+// @version	0.0.1a
+// @description	Auto-join script for 2015 Summer Steam Monster Minigame
+// @author	geekahedron
+// @match       http://example.org/
+// @grant	none
