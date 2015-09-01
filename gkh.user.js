afcf45f2808bbda7332a8acbf0be3e0f12bdd30d
geekahedron <d_j_@hotmail.com>
2015-09-01 14:51:50 -0400

    Update gkh.user.js

--- gkh.user.js
+++ gkh.user.js
@@ -4,7 +4,8 @@
 // @version	0.0.1a
 // @description	Auto-join script for 2015 Summer Steam Monster Minigame
 // @author	geekahedron
-// @match       http://example.org/
+// @match       http://utopia-game.com/wol/*
 // @updateURL   https://github.com/geekahedron/gopher-kingdom-haven/raw/master/gkh.user.js
 // @downloadURL https://github.com/geekahedron/gopher-kingdom-haven/raw/master/gkh.user.js
 // @grant	none
+// ==/UserScript==
